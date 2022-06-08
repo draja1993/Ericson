5b2ae0bd75fbf7a8809461fb5263341dbd16794b
draja1993 <rajadevabe93@gmail.com>
2022-06-08 18:15:14 +0530

    Tester Two add some code

--- src/org/ericson/EricsonADP.java
+++ src/org/ericson/EricsonADP.java
@@ -9,4 +9,8 @@ public class EricsonADP {
 	private void employeeID() {
 		System.out.println("Employee Id:RA6582");
 	}
+	
+	private void employeeSalary() {
+		System.out.println("Employee Salary:20k");
+	}
 }
